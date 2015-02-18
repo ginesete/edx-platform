e765616cb2a7b0066fe7203c4f09504853cd209e
Awais <awais.qureshi@arbisoft.com>
2015-02-18 16:21:49 +0500

    ECOM-1046 minor refactoring in code

--- lms/djangoapps/certificates/api.py
+++ lms/djangoapps/certificates/api.py
@@ -3,7 +3,6 @@ Certificates API views
 """
 
 import logging
-from opaque_keys import InvalidKeyError
 from django.utils.translation import ugettext as _
 from django.views.decorators.http import require_POST
 from courseware import grades
@@ -12,6 +11,7 @@ from util.json_request import JsonResponse, JsonResponseBadRequest
 from models import CertificateStatuses as cert_status, certificate_status_for_student
 from queue import XQueueCertInterface
 from opaque_keys.edx.keys import CourseKey
+from opaque_keys import InvalidKeyError
 
 log = logging.getLogger("edx.certificate")
 
@@ -45,9 +45,7 @@ def generate_user_cert(request, course_id):
     if not course:
         return JsonResponseBadRequest(_("Course is not valid"))
 
-    grade = grades.grade(student, request, course)
-
-    if not is_course_passed(course, grade):
+    if not is_course_passed(course, None, student, request):
         return JsonResponseBadRequest(_("You failed to pass the course."))
 
     xqueue = XQueueCertInterface()
@@ -86,7 +84,7 @@ def is_course_passed(course, grade_summary=None, student=None, request=None):
 def certificate_downloadable_status(student, course_key):
     """
     check the student existing certificates against a given course.
-    if status is not generating and downloadable then user can view the generate button.
+    if status is not generating and not downloadable or error then user can view the generate button.
 
     Args:
         student : user object
@@ -98,7 +96,7 @@ def certificate_downloadable_status(student, course_key):
 
     response_data = {
         'is_downloadable': False,
-        'is_generating': True if current_status['status'] == cert_status.generating else False,
+        'is_generating': True if current_status['status'] in [cert_status.generating, cert_status.error] else False,
         'download_url': None
     }
 
